cacb6c6d7ab89ce75a8cd6895bc91992f7545be6
PETERIPAII.C <107237414+Peteripai@users.noreply.github.com>
2024-07-16 18:34:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var typed = new Typed('#typed-output', {
-      strings: ["Hi there, Your Success starts here , we offer web development services ",],
+      strings: ["Hi there,Welcome! Your Success starts here , we offer web development services ",],
       typeSpeed: 60,
       backSpeed: 60,
       loop: true
