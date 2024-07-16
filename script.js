590da3a3d8118d346bd0d5eeeab53a83965fd4a2
PETERIPAII.C <107237414+Peteripai@users.noreply.github.com>
2024-07-16 18:33:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
       strings: ["Hi there, Your Success starts here , we offer web development services ",],
       typeSpeed: 60,
       backSpeed: 60,
-      loop: false
+      loop: true
     });
   });
   
